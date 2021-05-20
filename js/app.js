e958127d766b16501834d654c7598e984965adf3
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-20 07:52:06 -0400

    Update

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const hamburgerBtn = document.querySelector(".top-section__hamburger-btn");
+
+hamburgerBtn.addEventListener("click", () => {
+    hamburgerBtn.classList.toggle("active")
+    hamburgerBtn.nextElementSibling.classList.toggle("open")
+})
