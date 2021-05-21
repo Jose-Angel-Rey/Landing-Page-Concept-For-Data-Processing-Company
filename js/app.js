62ea02cdce6e00e1537a6192e4bba92a664d0a42
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-21 07:11:08 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+// Hamburger menu
 const hamburgerBtn = document.querySelector(".top-section__hamburger-btn");
 
 hamburgerBtn.addEventListener("click", () => {
-    hamburgerBtn.classList.toggle("active")
-    hamburgerBtn.nextElementSibling.classList.toggle("open")
-})
+  hamburgerBtn.classList.toggle("active");
+  hamburgerBtn.nextElementSibling.classList.toggle("open");
+});
