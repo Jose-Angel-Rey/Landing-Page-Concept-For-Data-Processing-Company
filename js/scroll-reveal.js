47c4aa246b6afc9aec349503c7dc7c9dd9b73c71
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-28 14:39:10 -0400

    Update

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -32,67 +32,67 @@ ScrollReveal().reveal(".your-choice-article", {
 });
 ScrollReveal().reveal(".increase-your-traffic-section", {
   duration: 3000,
-  delay: 2000,
+  delay: 1000,
   origin: "left",
   distance: "10rem"
 });
 ScrollReveal().reveal(".increase-your-traffic-section__graphic", {
   duration: 3000,
-  delay: 2000,
+  delay: 1000,
   origin: "right",
   distance: "10rem"
 });
 ScrollReveal().reveal(".ways-to-save-money-section", {
   duration: 3000,
-  delay: 2500,
+  delay: 1500,
   origin: "left",
   distance: "10rem"
 });
 ScrollReveal().reveal(".ways-to-save-money-article", {
   duration: 3000,
-  delay: 2500,
+  delay: 1500,
   origin: "bottom",
   distance: "5rem"
 });
 ScrollReveal().reveal(".create-your-next-project-section", {
   duration: 3000,
-  delay: 3000,
+  delay: 1000,
   origin: "right",
   distance: "10rem"
 });
 ScrollReveal().reveal(".create-your-next-project-section__illustration", {
   duration: 3000,
-  delay: 3000,
+  delay: 1000,
   origin: "bottom",
   distance: "15rem"
 });
 ScrollReveal().reveal(".invest-property-section", {
   duration: 3000,
-  delay: 3500,
+  delay: 1500,
   origin: "left",
   distance: "10rem"
 });
 ScrollReveal().reveal(".invest-property-section__img", {
   duration: 3000,
-  delay: 3500,
+  delay: 1500,
   origin: "right",
   distance: "10rem"
 });
 ScrollReveal().reveal(".finding-information-section", {
   duration: 3000,
-  delay: 3500,
+  delay: 1500,
   origin: "right",
   distance: "10rem"
 });
 ScrollReveal().reveal(".finding-information-section__img", {
   duration: 3000,
-  delay: 3500,
+  delay: 1500,
   origin: "right",
   distance: "10rem"
 });
 ScrollReveal().reveal(".finding-information-article", {
   duration: 3000,
-  delay: 4000,
+  delay: 1000,
   origin: "bottom",
   distance: "5rem"
 });
@@ -100,7 +100,7 @@ ScrollReveal().reveal(".finding-information-article", {
 // Aside CTA section
 ScrollReveal().reveal(".aside-cta-section-wrapper", {
   duration: 3000,
-  delay: 4000,
+  delay: 1000,
   origin: "bottom",
   distance: "15rem"
 });
@@ -108,13 +108,13 @@ ScrollReveal().reveal(".aside-cta-section-wrapper", {
 // Main footer
 ScrollReveal().reveal(".main-footer", {
   duration: 3000,
-  delay: 4000,
+  delay: 1000,
   origin: "left",
   distance: "10rem"
 });
 ScrollReveal().reveal(".attribution", {
   duration: 3000,
-  delay: 3000,
+  delay: 1500,
   origin: "right",
   distance: "10rem"
 });
